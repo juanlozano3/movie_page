7042a52b80dbbeac32c1c7c660dbe536bf2d5aca
Juan Lozano <juanl@MacBook-Air-69.local>
2025-02-22 15:00:06 -0500

    Movie-card done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import './App.css';5
-
 function App() {
+
 
   return (
     <>
-
 
     </>
   )
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -17,6 +17,9 @@ function MovieCard({movie}){
             <div className="movie-info">
                 <h3>{movie.title}</h3>
                 <p>{movie.release_date}</p>
+                </div>
         </div>
     )
 }
+
+export default MovieCard;
